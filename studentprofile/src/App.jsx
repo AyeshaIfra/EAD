02e60c361d6f2facd7a63eb4ac6fc3e2fbcb18c1
AyeshaIfra <ifraayesha576@gmail.com>
2026-04-01 15:44:28 +0530

    done

---
+++ studentprofile/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StudentCard } from './StudentCard'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div style={{display:"flex",justifyContent:"center", gap:"20px", flexWrap:"wrap"}}>
+    <StudentCard image="/images.jpg" name="Ayesha" branch="IT" year="2026"/>
+    <StudentCard image="/images.jpg" name="Bhavita" branch="IT" year="2026"/>
+    <StudentCard image="/images.jpg" name="Srujana" branch="IT" year="2026"/>
+    </div>
+  )
+}
+
+export default App
